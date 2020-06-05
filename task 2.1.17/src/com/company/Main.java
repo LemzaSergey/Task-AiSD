a5aba5b76fb261045bc2237c553c9266d866078e
LemzaSergey <55410819+LemzaSergey@users.noreply.github.com>
2020-06-06 00:50:56 +0300

    task 2.1.17

--- task 2.1.17/src/com/company/Main.java
+++ task 2.1.17/src/com/company/Main.java
@@ -9,21 +9,6 @@ import com.company.Tariffs;
 public class Main {
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.ROOT);
-        TariffsForDirections t1 = new TariffsForDirections(7, "ru", 5);
-        TariffsForDirections t2 = new TariffsForDirections(1, "ua", 9);
-        //Tariffs plan = new Tariffs(t1);
-        //TariffsForDirections.printTariffsForDirections(t1);
-        //Tariffs.printTariffs(plan);
-        //List<TariffsForDirections> tariffsPlan = new ArrayList<>();
-        //tariffsPlan.add(t1);
-        List<TariffsForDirections> tariffsPlan = new ArrayList<>();
-        tariffsPlan = Tariffs.addTariffs(t1, tariffsPlan);
-        tariffsPlan = Tariffs.addTariffs(t2, tariffsPlan);
-        tariffsPlan = Tariffs.removeTariffs(tariffsPlan,0);
-        //tariffsPlan = Tariffs.removeTariffs(tariffsPlan,0);
-        tariffsPlan = Tariffs.setTariffs(t2,tariffsPlan,0);
-        Tariffs.printTariffs(tariffsPlan, 0);
 
 
         if (args.length > 0) {
---
+++ task 2.1.17/working_window.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_101
+set JAVA_HOME=C:\Program Files\Java\jdk-13.0.1
+set PATH=%JAVA_HOME%\bin;%PATH%
+cd out\production\task 2.1.17
+
+java com.company.Main -window
+
